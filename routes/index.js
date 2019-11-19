8fb0f3d4f0468e09338d8f7f139454bbcbc4bacb
artluxint <adgk.dev@gmail.com>
2019-11-19 23:20:06 +0100

    favicon quickfix - routes to 204status

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var CompletedSquiggle = require("../models/completeSquiggle");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index");
 });
 
 router.get("/gallery", function(req, res, next) {
