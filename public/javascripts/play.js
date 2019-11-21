e9b37fd6e20f11352479e3f09e011b945aae89e1
artluxint <adgk.dev@gmail.com>
2019-11-21 18:59:22 +0100

    multitouch zoom test

--- public/javascripts/play.js
+++ public/javascripts/play.js
@@ -269,24 +269,24 @@ canvas.addEventListener("touchmove", e => touchPos(e), { passive: false });
 
 //
 // Prevent scrolling when touching the canvas
-document.body.addEventListener(
-  "touchstart",
-  function(e) {
-    if (e.target == canvas && e.touches.length < 2) {
-      e.preventDefault();
-    }
-  },
-  { passive: false }
-);
-document.body.addEventListener(
-  "touchend",
-  function(e) {
-    if (e.target == canvas && e.touches.length < 2) {
-      e.preventDefault();
-    }
-  },
-  { passive: false }
-);
+// document.body.addEventListener(
+//   "touchstart",
+//   function(e) {
+//     if (e.target == canvas && e.touches.length < 2) {
+//       e.preventDefault();
+//     }
+//   },
+//   { passive: false }
+// );
+// document.body.addEventListener(
+//   "touchend",
+//   function(e) {
+//     if (e.target == canvas && e.touches.length < 2) {
+//       e.preventDefault();
+//     }
+//   },
+//   { passive: false }
+// );
 document.body.addEventListener(
   "touchmove",
   function(e) {
