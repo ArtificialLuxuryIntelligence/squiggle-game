4b8e50dc7d0fc1d1ff05047895ee6183f458c595
artluxint <adgk.dev@gmail.com>
2019-11-22 17:26:05 +0100

    random colour generator

--- public/javascripts/newsquiggle2.js
+++ public/javascripts/newsquiggle2.js
@@ -1,6 +1,9 @@
+//same as play.js with:
+//different submit function
+//different load function (no squiggle rendered)
+
 const body = document.body;
 const canvas = document.getElementById("canvas");
-const undo = document.getElementById("undo");
 const restart = document.getElementById("restart");
 const form = document.getElementById("submit-form");
 const input = document.getElementById("hiddenField");
@@ -12,31 +15,12 @@ let points = [];
 let section = 0;
 let squiggle;
 
-//colour variables
+//colour variable (from database)
 
-//maybe save these in an object with
-// squiggle colours will have corresponding stroke colour (even if it's not identical)
-//generate random squiggleColour then generate strokeColour from the REMAINING colours
-let squiggleColour, strokeColour;
-const generateColourScheme = function(obj) {
-  var keys = Object.keys(obj);
-  let prop = obj[keys[(keys.length * Math.random()) << 0]];
-  squiggleColour = prop.sqCol;
-  let randomStrokeIndex = (prop.stCol.length * Math.random()) << 0;
-  strokeColour = prop.stCol[randomStrokeIndex];
-};
-const colours = {
-  red: { sqCol: "red", stCol: ["orange", "yellow"] },
-  blue: { sqCol: "blue", stCol: ["pink", "purple"] }
-};
+const strokeColours = [];
 
-generateColourScheme(colours);
-
-// let squiggleColour =
-//   squiggleColours[Math.floor(Math.random() * squiggleColours.length)];
-// let strokeColour =
-//   strokeColours[Math.floor(Math.random() * strokeColours.length)];
-
+let squiggleColour = "#36494E";
+let strokeColour = "#9E2A2B";
 let fillColour = "white";
 
 //context styling
@@ -80,15 +64,17 @@ function draw() {
       ctx.stroke();
       points[section].push({ x: mouse.x, y: mouse.y });
     } else {
-      //smoothing
+      //smoothing radius
       let x1 = points[section][points[section].length - 1].x;
       let y1 = points[section][points[section].length - 1].y;
       let x2 = mouse.x;
       let y2 = mouse.y;
+      //if distance is less that some set distance (chain)
+      // x = x1
 
       //disatnce between points
       let dist = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-      //angle between points
+      //angle between drawFromPoints
       let alpha = Math.atan2(x2 - x1, y2 - y1);
 
       if (dist < chain) {
@@ -114,36 +100,6 @@ const backgroundFill = () => {
   ctx.fillStyle = fillColour;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.fill();
-};
-
-const drawFromPoints = (collection, strokecolour) => {
-  ctx.strokeStyle = strokecolour;
-  for (let i = 0; i < collection.length; i++) {
-    ctx.moveTo(collection[i][0].x, collection[i][0].y);
-    ctx.beginPath();
-    for (let j = 0; j < collection[i].length; j++) {
-      // linear;
-      ctx.lineTo(collection[i][j].x, collection[i][j].y);
-
-      // // //quadratic line for smoothing [ note: loop to collection[i].length-2]
-      // let a = collection[i][j + 1].x;
-      // let b = collection[i][j + 1].y;
-      // //
-      // // let a = (collection[i][j].x + collection[i][j + 1].x) / 2;
-      // // let b = (collection[i][j].y + collection[i][j + 1].y) / 2;
-      // ctx.quadraticCurveTo(
-      //   a,
-      //   b,
-      //   collection[i][j + 2].x,
-      //   collection[i][j + 2].y
-      // );
-      ctx.moveTo(collection[i][j].x, collection[i][j].y);
-      ctx.stroke();
-      ctx.closePath();
-    }
-
-    ctx.closePath();
-  }
 };
 
 // Event Handlers
@@ -225,7 +181,6 @@ const undoHandler = points => {
 
 const resetCanvas = () => {
   backgroundFill();
-  drawFromPoints(squiggle, squiggleColour);
   ctx.strokeStyle = strokeColour;
   section = 0;
   points = [];
@@ -239,12 +194,8 @@ canvas.addEventListener("mousemove", e => mousePos(e));
 // undo.addEventListener("click", () => undoHandler(points));
 restart.addEventListener("click", resetCanvas);
 
-//submit completeSquiggle
 form.addEventListener("submit", async () => {
-  let dataURL = await canvas.toDataURL();
-  input.value = dataURL;
-  // let data = await JSON.stringify(points);
-  // input.value = JSON.stringify(points);
+  input.value = JSON.stringify(points);
 });
 
 //fetch squiggle
@@ -254,13 +205,10 @@ const fetchSquiggle = async () => {
   return JSON.parse(json.line);
 };
 
-/// render squiggle
+/// ..
 window.addEventListener("load", async () => {
   backgroundFill();
-  squiggle = await fetchSquiggle();
-  drawFromPoints(squiggle, squiggleColour);
-  let dataURL = await canvas.toDataURL();
-  input2.value = dataURL;
+
   ctx.strokeStyle = strokeColour;
 });
 
@@ -276,6 +224,15 @@ function touchPos(e) {
   touch.y = e.touches[0].clientY - rect.top;
 }
 
+function touchdraw() {
+  if (isDrawing) {
+    ctx.lineTo(touch.x, touch.y);
+    ctx.stroke();
+    if (points[section]) {
+      points[section].push({ x: touch.x, y: touch.y });
+    }
+  }
+}
 function touchdraw() {
   if (isDrawing) {
     if (points[section].length == 0) {
