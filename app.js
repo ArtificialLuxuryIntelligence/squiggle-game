5668dbcf63ed73878ecf975f854e1609d3d7342c
ali <adgk.dev@gmail.com>
2020-03-13 15:56:57 +0100

    cors now allows all origins

--- app.js
+++ app.js
@@ -20,25 +20,27 @@ var app = express();
 //   origin: 'http://squiggle-game.herokuapp.com/'
 // }));
 
-var allowedOrigins = [
-  "http://localhost:3000",
-  "http://squiggle-game.herokuapp.com/"
-];
+// var allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://squiggle-game.herokuapp.com"
+// ];
 
-app.use(
-  cors({
-    origin: function(origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    }
-  })
-);
+// app.use(
+//   cors({
+//     origin: function(origin, callback) {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     }
+//   })
+// );
+
+app.use(cors());
 
 // connect to database
 mongoose.connect(process.env.DB, { useNewUrlParser: true });
